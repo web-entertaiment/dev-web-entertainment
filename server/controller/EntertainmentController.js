35d09809e53597a2decda11e77cdf5a86d29608d
abdulmujibf <abdulmujib628@gmail.com>
2021-02-05 00:07:56 +0700

    getAnime harus Search

--- server/controller/EntertainmentController.js
+++ server/controller/EntertainmentController.js
@@ -1,6 +1,17 @@
+const axios = require('axios')
+
 
 class EntertainmentController {
-
+    static getAnime(req, res, next){
+        let query = req.query.search
+        axios.get(`https://api.jikan.moe/v3/search/anime/?q=${query}&page=1`)
+        .then(response => {
+            res.status(200).json(response.data)
+        })
+        .catch(err => {
+            next(err)
+        })
+    }
 
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userRouter = require('./userRouter')
-// const entertainmentRouter = require('./todoRouter')
+const entertainmentRouter = require('./entertainmentRouter')
 
 router.use('/user', userRouter)
-// router.use('/entertainment', entertainmentRouter)
+// router.get('/favourite', FavouriteController)
+router.use('/entertainment', entertainmentRouter)
 
 module.exports = router
