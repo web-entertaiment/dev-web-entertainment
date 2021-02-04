a8d7fb9a499a640b2e83ba434d94faaa5ae93d77
ahmad-akbar <ahmd.akbr25@gmail.com>
2021-02-05 01:55:40 +0800

    add 3rd party comic marvel

--- server/routes/entertainmentRouter.js
+++ server/routes/entertainmentRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const EntertainmentController = require('../controller/EntertainmentController')
 
-
+router.get('/comics', EntertainmentController.getComics)
 
 module.exports = router
